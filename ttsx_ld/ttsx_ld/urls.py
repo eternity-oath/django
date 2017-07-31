7c855a96463ec9dadc59272dc63d65ef002388b3
lidi <eternity_oath@163.com>
2017-07-31 09:29:56 +0800

    创建项目

--- ttsx_ld/ttsx_ld/urls.py
+++ ttsx_ld/ttsx_ld/urls.py
@@ -1,4 +1,4 @@
-"""ttsx_lidi URL Configuration
+"""ttsx_ld URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- ttsx_ld/ttsx_ld/wsgi.py
+++ ttsx_ld/ttsx_ld/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ttsx_lidi project.
+WSGI config for ttsx_ld project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ttsx_lidi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ttsx_ld.settings")
 
 application = get_wsgi_application()
